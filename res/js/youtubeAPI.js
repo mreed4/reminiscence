9f4dd7952f7d3ea0fa44e7fb06daa690f6e00c54
Matthew <78713317+mreed4@users.noreply.github.com>
2023-02-19 11:44:54 -0800

    Major update

--- res/js/youtubeAPI.js
+++ res/js/youtubeAPI.js
@@ -4,18 +4,27 @@ import { config } from "./config.js";
 const key = config.key;
 
 const inputURL = document.querySelector("#video-url");
-const ol = document.querySelector("ol");
-const h1 = document.querySelector("h1");
+const ol = document.querySelector("#comments-list");
+const h2 = document.querySelector("h2");
+const getCommentsBtn = document.querySelector("#get-comments");
+const h1Count = document.querySelector("h1 span");
+const pCount = document.querySelector("p span");
+
+const youtubeOrder = "relevance"; // relevance, time, orderUnspecified
+const sortMethod = "comment-length"; // likes, date, replies, comment-length
+const dateSort = "oldest"; // newest, oldest
 
 const apiBase = "https://www.googleapis.com/youtube/v3";
 
-let videoId;
+let videoId = "" || youtubeParser("https://youtu.be/n2MtEsrcTTs");
 const commentAmount = 100;
+h1Count.textContent = commentAmount || 20;
+pCount.textContent = commentAmount || 20;
 
-inputURL.addEventListener(
-  "change",
-  (event) => {
-    videoId = youtubeParser(event.target.value);
+getCommentsBtn.addEventListener(
+  "click",
+  () => {
+    videoId = youtubeParser(inputURL.value);
     getVideoData();
     inputURL.value = "";
     ol.innerHTML = "";
@@ -31,7 +40,7 @@ function getVideoTitle() {
         if (dat) {
           // console.log(dat);
           const videoTitle = dat.items[0].snippet.title;
-          h1.textContent = videoTitle;
+          h2.textContent = videoTitle;
         }
       })
       .catch((e) => console.log(e));
@@ -40,22 +49,52 @@ function getVideoTitle() {
 
 function getComments() {
   if (videoId) {
-    fetch(`${apiBase}/commentThreads?part=snippet&maxResults=${commentAmount}&order=relevance&videoId=${videoId}&key=${key}`)
+    const params = `part=snippet&maxResults=${commentAmount}&order=${youtubeOrder}&videoId=${videoId}&key=${key}`;
+    fetch(`${apiBase}/commentThreads?${params}`)
       .then((res) => res.json())
       .then((dat) => {
         if (dat) {
           console.log(dat);
           dat.items
             .sort((a, b) => {
-              return b.snippet.topLevelComment.snippet.likeCount - a.snippet.topLevelComment.snippet.likeCount;
+              if (sortMethod === "likes") {
+                return b.snippet.topLevelComment.snippet.likeCount - a.snippet.topLevelComment.snippet.likeCount;
+              } else if (sortMethod === "date") {
+                if (dateSort === "oldest") {
+                  return b.snippet.topLevelComment.snippet.publishedAt - a.snippet.topLevelComment.snippet.publishedAt;
+                } else if (dateSort === "newest") {
+                  return a.snippet.topLevelComment.snippet.publishedAt - b.snippet.topLevelComment.snippet.publishedAt;
+                } else {
+                  return;
+                }
+              } else if (sortMethod === "replies") {
+                return b.snippet.totalReplyCount - a.snippet.totalReplyCount;
+              } else if (sortMethod === "comment-length") {
+                return b.snippet.topLevelComment.snippet.textDisplay.length - a.snippet.topLevelComment.snippet.textDisplay.length;
+              } else {
+                return b.snippet.topLevelComment.snippet.textDisplay.length - a.snippet.topLevelComment.snippet.textDisplay.length;
+              }
             })
             .map((item) => {
               const comment = item.snippet.topLevelComment.snippet;
               const likes = comment.likeCount;
               const text = comment.textDisplay;
               const date = Date.parse(comment.publishedAt) / 1000;
+              const replies = item.snippet.totalReplyCount;
+              const author = comment.authorDisplayName;
+              const authorUrl = comment.authorChannelUrl;
 
-              const li = `<li>${text} ${getRelativeTime(date)} (${likes})</li>`;
+              const li = `<li>
+                <div class="comment-wrap">
+                  <div class="comment-text">${text}</div>
+                  <div class="comment-data">
+                    <span><a href=${authorUrl}>${author}</a></span>
+                    <span>${getRelativeTime(date)}</span>
+                    <span>${likes} Likes</span>
+                    <span>${replies} Replies</span>
+                    </div>
+                </div>
+              </li>`;
 
               // console.log(comment.length);
               if (!text.includes("<br>")) {
