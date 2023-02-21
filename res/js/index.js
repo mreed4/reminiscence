dab8556811a779c306d9c972ead9726e3ed61908
Matthew <78713317+mreed4@users.noreply.github.com>
2023-02-20 19:46:55 -0800

    Misc. changes

--- res/js/index.js
+++ res/js/index.js
@@ -1,5 +1,20 @@
 import { getVideoData } from "./youtubeAPI.js";
+import { youtubeParser } from "./helpers.js";
 
-window.onload = function () {
-  getVideoData();
-};
+const getCommentBtn = document.querySelector("#get-comments");
+const inputURL = document.querySelector("#video-url");
+const video = inputURL;
+
+const commentAmount = 100;
+const h1Count = document.querySelector("h1 span");
+const pCount = document.querySelector("p span");
+
+h1Count.textContent = commentAmount || 20;
+pCount.textContent = commentAmount || 20;
+
+function getComments(event) {
+  getVideoData(youtubeParser(video.value), commentAmount);
+  inputURL.value = "";
+}
+
+getCommentBtn.addEventListener("click", getComments);
